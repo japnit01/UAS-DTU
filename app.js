6ab122ee3286ba4c84bd504a7e042632d6a48d38
japnit01 <japnit2012@gmail.com>
2021-01-26 09:49:34 +0530

    basic framework

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+let express = require("express"),
+    bodyParser = require("body-parser"),
+    nodemon  = require("nodemon"),
+    app = express();
+
+const port = 80;
+
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",(req,res)=>{
+    res.render("home")
+});
+
+app.get("/hangar",(req,res)=>{
+    res.render("hangar")
+})
+app.listen(port,()=>{
+    console.log("app is listening");
+});
 
